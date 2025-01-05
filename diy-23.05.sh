59496a316febba5a01352b095c7a9b9700f457c8
yuan93 <32140220+yuan93@users.noreply.github.com>
2025-01-05 09:47:54 +0000

    Update diy-23.05.sh

--- diy-23.05.sh
+++ diy-23.05.sh
@@ -259,6 +259,7 @@ clone_dir https://github.com/vernesong/OpenClash luci-app-openclash
 # diy
 clone_all https://github.com/4IceG/luci-app-timecontrol
 clone_all https://github.com/gaoyaxuan/luci-app-pushbot
+clone_all https://github.com/chenmozhijin/turboacc
 
 # Themes
 git_clone https://github.com/kiddin9/luci-theme-edge
